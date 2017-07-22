0b73a9b7504814ff6b23d780dc7fa350c929b6d5
qiushi <12qiushi@163.com>
2017-07-22 14:02:16 +0800

    ff

--- beiju.py
+++ beiju.py
@@ -1,70 +1,9 @@
 #
 # _*_ coding:UTF-8 _*_
-import time
-from control import *
-from pop import bubble
 from screen import closeScreen,openSecreen
 from zuotian import Zuotian
+from dahua import *
 
-def message(str) :
-    bubble.startBubble(u'挂机',str+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),0)
-
-def sheyaoxiang():
-
-    time.sleep(3)
-    mouse_click(891,573)
-    time.sleep(1)
-    mouse_click(1249, 863)
-    time.sleep(2)  
-    mouse_dclick(1162, 424)
-    time.sleep(2)
-    mouse_click(1026, 636)
-    time.sleep(2)
-    mouse_click(1320, 298)
-    time.sleep(2)
-    mouse_click(980, 313)
-    time.sleep(2)
-    mouse_move(0,0)
-
-
-def log(str):
-    print str + time.strftime('%H:%M:%S',time.localtime(time.time()))
-
-def checkZhandou():
-    log(u'抓取屏幕')
-    im0 =ImageGrab.grab((478,228,484,234))
-    color = im0.getpixel((2,2))
-    log(u'抓取屏幕成功')
-    if '%d%d%d'%color=='239188109':
-        message('正在战斗'.decode('utf8'))
-        return True
-    else:
-        print time.localtime()
-        message('离开战斗'.decode('utf8'))
-        return False
-def checkXiang():
-    log(u'抓取屏幕')
-    imxiang = ImageGrab.grab((1037,266,1086,288))
-    color = imxiang.getpixel((17,12))
-    log(u'抓取屏幕成功')
-    if '%d%d%d'%color=='234113111':
-        return True
-    else:
-        return False
-def checkIsDahua():
-    log(u'抓取屏幕')
-    imcheckopen = ImageGrab.grab((413,911,417,918))
-    color = imcheckopen.getpixel((1,1))
-    log(u'抓取屏幕成功')
-    if '%d'*3%color=='232212180':
-        message(u'当前在大话中')
-        return True
-    else:
-        print '%d'*3%color
-        message(u'当前不在大话中')
-        return False
-
-from PIL import Image, ImageGrab
 if __name__ == "__main__":
 
     is_zuotian = False
@@ -76,7 +15,8 @@ if __name__ == "__main__":
             if not checkZhandou():
 
                 #判断是否可以做天
-                if int(time.strftime('%H',time.localtime(time.time()))) == 5  and (not is_zuotian):
+                if int(time.strftime('%H',time.localtime(time.time()))) == 6 and (not is_zuotian):
+                    message('领任务昨天'.decode('utf8'))
                     #进入昨天逻辑
                     Zuotian().go()
                     is_zuotian = True
--- zuotian.py
+++ zuotian.py
@@ -1,15 +1,7 @@
 # _*_ coding:UTF-8 _*_
-import sys,os
 from PIL import Image,ImageDraw,ImageEnhance,ImageFilter
-import io
-from control import *
-from pop import bubble
-from screen import closeScreen,openSecreen
 from PIL import Image, ImageGrab
-
-def message(str) :
-    bubble.startBubble(u'挂机',str+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),0)
-
+from dahua import *
 
 def twoValue(image):
     draw = ImageDraw.Draw(image)
@@ -23,23 +15,6 @@ def twoValue(image):
             else:
                 draw.point((x,y),0)
 
-#
-#
-# image = Image.open('C:\\Users\\admin\\Desktop\\beiju\\7.png')
-# enhancer = ImageEnhance.Contrast(image)
-# im = enhancer.enhance(3)
-# #将图片转换成灰度图片
-# image = image.convert("L")
-#
-# image.show()
-# twoValue(image)
-# image.show()
-#
-# image.save('2.jpg')
-
-import time
-def log(str):
-    print str + time.strftime('%H:%M:%S',time.localtime(time.time()))
 
 class Zuotian:
     def __init__(self):
@@ -58,25 +33,12 @@ class Zuotian:
 
         time.sleep(5)
         mouse_click(1171,618)
-    def checkZhandou(self):
-        log(u'抓取屏幕')
-        im0 =ImageGrab.grab((478,228,484,234))
-        color = im0.getpixel((2,2))
-        log(u'抓取屏幕成功')
-        if '%d%d%d'%color=='239188109':
-            message('正在战斗'.decode('utf8'))
-            return True
-        else:
-            print time.localtime()
-            message('离开战斗'.decode('utf8'))
-            return False
+
     def checkIsTian(self):
         image = ImageGrab.grab((1161,349,1359,371))
         if '%d%d%d'%image.getpixel((3,4))=='255255255' and '%d%d%d'%image.getpixel((2,4))!='255255255' and '%d%d%d'%image.getpixel((3,13))=='255255255':
             return True
         return False
-
-
 
     def go_back_beiju(self):
         mouse_click(426,197)
@@ -93,7 +55,7 @@ class Zuotian:
     def auto_click_mission(self):
         while True:
 
-            if not self.checkZhandou():
+            if not checkZhandou():
                 if self.checkIsTian():
                     message('在任务中'.decode('utf8'))
                     self.not_have_mission = 0
